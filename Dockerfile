df03a2e7299a10243c86926506fe3ebfa3061248
root <root@master.demo.lab>
2023-07-24 17:28:49 +0530

    second commit myCode added

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python 
 RUN mkdir /hello
 COPY file.py /hello/
+COPY myCode.py /hello/
 CMD ["python3","/hello/file.py"]
+CMD ["python3","/hello/myCode.py"]
---
+++ myCode.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+
+if a==b:
+	print(f"{a} and {b} are equal")
+elif a>b:
+	print(f"{a} is greater than {b}")
+else:
+	print(f"{b} is greater than {a}")
